0ede87a40893349769046bf5b1b85b2630e2f5d9
Michael Mangus <mjmangus16@yahoo.com>
2019-07-15 14:40:13 -0400

    initial commit

---
+++ accounts/accounts-router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
